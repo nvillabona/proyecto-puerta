f81c7e353a0c2347880935acf19b5e7f1191cbfc
nvillabona <nvillabona97@gmail.com>
2019-10-22 10:38:12 -0500

    se ordena codigo y se corrige error banner derecho

--- display.php
+++ display.php
@@ -6,10 +6,10 @@
     <head>
     <title>Arduino Ethernet Database</title>
     <!-- id izq                       ----------------------------------------------------- -->
-<div class="izq">
-<link rel="stylesheet" href="style.css">
-<script src="functions.js"></script>
-<body onload="ConteoRegresivo()">
+    <div class="izq">
+    <link rel="stylesheet" href="style.css">
+    <script src="functions.js"></script>
+    <body onload="ConteoRegresivo()">
     
     </head>
     <body>
@@ -23,37 +23,38 @@
     <td class="table_titles">Heat_index</td>
     </tr>
     <?php
-    include('connection.php');
-    $result = mysqli_query($con,'SELECT * FROM data ORDER BY id DESC');
-    // Process every record
-    $oddrow = true;
-    while($row = mysqli_fetch_array($result))
-    {
-    if ($oddrow)
-    {
-    $css_class=' class="table_cells_odd"';
-    }
-    else
-    {
-    $css_class=' class="table_cells_even"';
-    }
-    $oddrow = !$oddrow; 
-    echo "<tr>";
-    echo "<td '.$css_class.'>" . $row['id'] . "</td>";
-    echo "<td '.$css_class.'>" . $row['event'] . "</td>";
-    echo "<td '.$css_class.'>" . $row['temperature'] . "</td>";
-    echo "<td '.$css_class.'>" . $row['humidity'] . "</td>";
-    echo "<td '.$css_class.'>" . $row['heat_index'] . "</td>";
-    echo "</tr>"; 
-    }
-     
-    // Close the connection
-    mysqli_close($con);
+      include('connection.php');
+      //obtain data from db
+      $result = mysqli_query($con,'SELECT * FROM data ORDER BY id DESC');
+      // Process every record
+      $oddrow = true;
+      while($row = mysqli_fetch_array($result))
+      {
+      if ($oddrow)
+      {
+      $css_class=' class="table_cells_odd"';
+      }
+      else
+      {
+      $css_class=' class="table_cells_even"';
+      }
+      $oddrow = !$oddrow; 
+      echo "<tr>";
+      echo "<td '.$css_class.'>" . $row['id'] . "</td>";
+      echo "<td '.$css_class.'>" . $row['event'] . "</td>";
+      echo "<td '.$css_class.'>" . $row['temperature'] . "</td>";
+      echo "<td '.$css_class.'>" . $row['humidity'] . "</td>";
+      echo "<td '.$css_class.'>" . $row['heat_index'] . "</td>";
+      echo "</tr>"; 
+      }
+      
+      // Close the connection
+      mysqli_close($con);
     ?>
     </table>
 </div>
 <!-- class der ------------------------>
-    <div class="otro">
+    <div class="der">
     
  
     
@@ -61,24 +62,24 @@
       <h2>Sistema de seguridad </h2>
       <!-- <div id="contador"></div> -->
       <h4><i> </i></h4>
-      <?php
-      include('connection.php');
-      date_default_timezone_set('America/Bogota');
-      $date_fromDB = mysqli_query($con,'SELECT * FROM data ORDER by event DESC LIMIT 1 ');
-      
-      $date = strtotime("October 22, 2019 4:00 AM");
-      $remaining = $date - time();
-      $days_remaining = floor($remaining / 86400);
-      $hours_remaining = floor(($remaining % 86400) / 3600);
-      
-      echo "There are $days_remaining days and $hours_remaining hours left";
-      $timezone = date_default_timezone_get();
-      echo "The current server timezone is: " . $timezone;
-      echo "-- $date_fromDB";
-      
-    //   $date = date('m/d/Y h:i:s a', time());
-    //   echo $date;
-      ?>
+        <?php
+          include('connection.php');
+          date_default_timezone_set('America/Bogota');
+          $date_fromDB = mysqli_query($con,'SELECT * FROM data ORDER by event DESC LIMIT 1 ');
+          
+          $date = strtotime("October 22, 2019 4:00 AM");
+          $remaining = $date - time();
+          $days_remaining = floor($remaining / 86400);
+          $hours_remaining = floor(($remaining % 86400) / 3600);
+          
+          echo "There are $days_remaining days and $hours_remaining hours left";
+          $timezone = date_default_timezone_get();
+          echo "The current server timezone is: " . $timezone;
+          echo "-- $date_fromDB";
+          
+          //$date = date('m/d/Y h:i:s a', time());
+          //echo $date;
+        ?>
     
     </div>
     </body>
